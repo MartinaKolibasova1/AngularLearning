63d7fce22bd248bb8808a158d2a4904ee3fac98c
Martina Kolibasova <martina.kolibasova@exponea.com>
2018-08-08 09:31:30 +0200

    Debugging+Project3Section

---
+++ Project3Section/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: [],
+})
+
+export class ShoppingEditComponent {
+  constructor() { }
+}
