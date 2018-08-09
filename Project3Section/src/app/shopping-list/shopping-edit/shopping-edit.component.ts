cfd914d84a752f2ef89e75c36e64a68f598a4bfc
Martina Kolibasova <martina.kolibasova@exponea.com>
2018-08-09 13:28:45 +0200

    finish course project add ingredients from input

--- Project3Section/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Project3Section/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,5 +8,17 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+
   constructor() { }
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
