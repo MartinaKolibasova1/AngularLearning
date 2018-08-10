39ecae91d3f432c8600e805b41ff0dbf4bbfac29
Martina Kolibasova <martina.kolibasova@exponea.com>
2018-08-10 12:55:30 +0200

    Finish services section

--- services-start/src/app/account.service.ts
+++ services-start/src/app/account.service.ts
@@ -1,3 +1,8 @@
+import {LoggingService} from './logging.service';
+import {EventEmitter, Injectable} from '@angular/core';
+import {s} from '@angular/core/src/render3';
+
+@Injectable()
 export class AccountsService {
   accounts = [
     {
@@ -14,11 +19,16 @@ export class AccountsService {
     }
   ];
 
+  statusUpdated = new EventEmitter<string>();
+
+  constructor(private loggingservice: LoggingService) {}
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingservice.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
    this.accounts[id].status = status;
+   this.loggingservice.logStatusChange(status);
   }
 }
