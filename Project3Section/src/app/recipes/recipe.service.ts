0cd235ab7b27d1d0e8c3661663912213cffc8562
Martina Kolibasova <martina.kolibasova@exponea.com>
2018-08-10 15:11:38 +0200

    Course project services + updated notes

--- Project3Section/src/app/recipes/recipe.service.ts
+++ Project3Section/src/app/recipes/recipe.service.ts
@@ -1,24 +1,35 @@
-import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import { Recipe } from './recipe.model';
+import {EventEmitter, Injectable} from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles' +
-      '%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=1600&q=70',
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-' +
-      'grilled-mustard-rosemary-chicken-recipe-alt-main.jpg',
-    ),
-  ];
+    recipeSelected = new EventEmitter<Recipe>();
+    private recipes: Recipe[] = [
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles' +
+          '%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=1600&q=70',
+            [new Ingredient('meat', 1), new Ingredient('French fries', 20)],
+        ),
+        new Recipe(
+            'Another Test Recipe',
+            'This is simply a test',
+            'https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-' +
+                'grilled-mustard-rosemary-chicken-recipe-alt-main.jpg',
+            [new Ingredient('Buns', 2), new Ingredient('Meat', 1)],
+        ),
+    ];
 
-  getRecipes() {
-    return this.recipes.slice(); // return a copy of array
-  }
+    constructor(private slService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice(); // return a copy of array
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+      this.slService.addIngredients(ingredients);
+    }
 }
--- Project3Section/src/app/shopping-list/shopping-list.service.ts
+++ Project3Section/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,26 @@
-export class ShoppingListService {
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
 
+export class ShoppingListService {
+  ingredientAdded = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientAdded.emit(this.ingredients.slice());
+  }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients); // spread operator change array to list of single elements
+    this.ingredientAdded.emit(this.ingredients.slice());
+  }
 }
